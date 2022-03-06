94e6aa1213d7c6d6060be058b28eed2da7f5a8b8
Guanbo Bian <31135281+GBIAN2@users.noreply.github.com>
2022-03-06 15:18:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,57 @@
 from flask import Flask, render_template, request
+import json
+
+from basicGen import basicAlg
+
 
 app = Flask(__name__)
 
+
+# class_dict = {
+#     '1': 'CSPB 1300',
+#     '2': 'CSPB 2824',
+#     '3': 'CSPB 2270',
+#     '4': 'CSPB 3104',
+#     '5': 'CSPB 2400',
+#     '6': 'CSPB 3308',
+#     '7': 'CSPB 3155',
+#     '8': 'CSPB 3702',
+#     '9': 'CSPB 3022',
+#     '10': 'CSPB 4122',
+#     '11': 'CSPB 4502',
+#     '12': 'CSPB 2820',
+#     '13': 'CSPB 3403'
+# }
+
 class_dict = {
-    '1': 'CSPB 1300',
-    '2': 'CSPB 2824',
-    '3': 'CSPB 2270',
-    '4': 'CSPB 3104',
-    '5': 'CSPB 2400',
-    '6': 'CSPB 3308',
-    '7': 'CSPB 3155',
-    '8': 'CSPB 3702',
-    '9': 'CSPB 3022',
-    '10': 'CSPB 4122',
-    '11': 'CSPB 4502',
-    '12': 'CSPB 2820',
-    '13': 'CSPB 3403'
+    '1': 1300,
+    '2': 2824,
+    '3': 2270,
+    '4': 3104,
+    '5': 2400,
+    '6': 3308,
+    '7': 3155,
+    '8': 3702,
+    '9': 3022,
+    '10': 4122,
+    '11': 4502,
+    '12': 2820,
+    '13': 3403
 }
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         selected_list = request.form.getlist('mycheckbox')
-        remaining_class_key = [new for new in class_dict.keys() if new not in selected_list]
-        remaining_class = [class_dict[x] for x in remaining_class_key]
-        print("Classes remaining: ")
-        print(remaining_class)
-        return 'Done'
+        selected_class = [class_dict[x] for x in selected_list]
+        # remaining_class_key = [new for new in class_dict.keys() if new not in selected_list]
+        # remaining_class = [class_dict[x] for x in remaining_class_key]
+        to_take = basicAlg(selected_class)
+        print(selected_class)
+        print(to_take)
+        # return {"class to take": to_take}
+        return {"class to take next semester:": json.dumps(to_take)}
+
     return render_template('index.html')
 
 
