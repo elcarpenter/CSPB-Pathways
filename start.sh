fd50350674c285b50c2da9a730864e380ac74e82
Ryan Young <ryanyoung@Ryans-MacBook-Pro.local>
2022-04-22 16:36:09 -0500

    Updated requirements.txt and start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ source path/to/folder/CSPB-Pathways/venv/bin/activate
 # sql database not set up yet
 # export DATABASE_URL="postgresql://localhost/kinsey
 cd path/to/folder/CSPB-Pathways
+source venv/bin/activate
 export FLASK_APP=project.py
 export FLASK_DEBUG=1
 flask run
